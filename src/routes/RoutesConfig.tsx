e8388dd9a70dbc41641d75fda13fbbee3ed8daf2
Caio Cesar <105387511+caiocesardev@users.noreply.github.com>
2025-05-04 21:47:56 -0300

    feat: criação do componente CriarConta

--- src/routes/RoutesConfig.tsx
+++ src/routes/RoutesConfig.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home"; 
 import Login from "@/pages/Autenticacao/Login";
 import EsqueceuSenha from "@/pages/Autenticacao/EsqueceuSenha";
+import CriarConta from "@/pages/Autenticacao/CriarConta";
 
 
 const RoutesConfig = () => {
@@ -10,6 +11,7 @@ const RoutesConfig = () => {
       <Route path="/" element={<Home />} />
       <Route path="/autenticacao/login" element={<Login />} />
       <Route path="/autenticacao/esqueci-senha" element={<EsqueceuSenha />} />
+      <Route path="/autenticacao/criar-conta" element={<CriarConta />} />
     </Routes>
   );
 };
